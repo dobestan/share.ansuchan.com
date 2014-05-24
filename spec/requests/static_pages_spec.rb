095f6d02644336139aa471ed5ab662dda7fa8009
dobestan <dobestan@gmail.com>
2014-05-24 22:24:43 +0900

    tdd setting complete with spork and guard

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,8 +3,13 @@ require 'spec_helper'
 describe "StaticPages" do
   describe "Home Page" do
     it "should have the content 'hello world'" do
-      visit '/'
+      visit root_path
       expect(page).to have_content "hello world"
+    end
+
+    it "should have the content 'dobestan'" do
+      visit root_path
+      expect(page).to have_content "dobestan"
     end
   end
 end
