90cd8b8349d29317969f9eeae52c6fad5aa9f235
dobestan <dobestan@gmail.com>
2014-05-31 21:24:52 +0900

    add basic user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,27 @@
 class User < ActiveRecord::Base
+  before_create :create_remember_token
+
+  has_secure_password
+
+  validates :name, presence: true,
+                   uniqueness: { case_sensitive: false },
+                   length: { minimum: 4 }
+
+  validates :password, presence: true,
+                      length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+  validates :password_digest, presence: true
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 end
--- db/migrate/20140531115203_create_users.rb
+++ db/migrate/20140531115203_create_users.rb
@@ -1,8 +1,15 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string :name
+      t.string :password_digest
+      t.boolean :admin, default: false
+
+      t.string :remember_token
 
       t.timestamps
     end
+
+    add_index :users, :name, unique: true
   end
 end
