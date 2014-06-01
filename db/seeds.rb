9841ca72d9e1bef9e0871393a2331064925a349a
dobestan <dobestan@gmail.com>
2014-06-01 16:13:45 +0900

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,25 +6,34 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+# Admin User
 dobestan = User.create( name: "dobestan",
                         password: "dkstncks",
                         password_confirmation: "dkstncks",
                         admin: true)
 
+# Demo User
 demo = User.create( name: "demo",
                     password: "foobar",
                     password_confirmation: "foobar" )
 
 
-blog = Link.create( original: "http://blog.ansuchan.com",
-                    shorten: "blog",
-                    message: "Personal Tech Blog by @dobestan")
-
+# Homepage ( Link #1 )
 homepage = Link.create( original: "http://ansuchan.com",
                         shorten: "homepage",
                         message: "Personal Homepage by @dobestan" )
 
-private_link_example = Link.create( original: "http://naver.com",
-                                    shorten: "naver",
-                                    message: "Naver | 네이버",
-                                    public: false )
+# Tech Blog ( Link #2 )
+blog = Link.create( original: "http://blog.ansuchan.com",
+                    shorten: "blog",
+                    message: "Personal Tech Blog by @dobestan")
+
+# Github ( Link #3 )
+github = Link.create( original: "http://github.com/dobestan",
+                       shorten: "github",
+                       message: "Github by @dobestan")
+
+# Facebook ( Link #4 )
+facebook = Link.create( original: "http://facebook.com/dobestan",
+                        shorten: "facebook",
+                        message: "Facebook")
